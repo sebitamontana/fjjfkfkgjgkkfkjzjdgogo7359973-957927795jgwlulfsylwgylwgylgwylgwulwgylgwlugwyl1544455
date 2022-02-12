e4a0c40c1fa43cfab8cd3c198e7c93db7b8c7efa
sebitamontana <93792473+sebitamontana@users.noreply.github.com>
2022-02-12 20:32:37 -0300

    Add files via upload

---
+++ assets/preload/stages/acto.lua
@@ -0,0 +1,19 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('actostage', 'actostage', -616, -186);
+	addLuaSprite('actostage', false);
+	close(true)
+
+end
+
+function onBeatHit()
+
+end
+
+function onStepHit()
+
+end
+
+function onUpdate()
+
+end
