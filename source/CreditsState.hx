d5ad525f1c2424bfafa4c3d14f8d027acb5ec07b
sebitamontana <93792473+sebitamontana@users.noreply.github.com>
2022-02-15 18:57:14 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -48,6 +48,10 @@ class CreditsState extends MusicBeatState
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
 			['Psych Engine Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['Android Port'],
+			['patricio Montana skr',		    'majigsaw',		    'porter ir the mod',	 'https://youtube.com/channel/UCMr5sqTwZ7CiWFGYF3LcSFA',	'0xFFC30085'],
+			['mod'],
+			['lemoniado mod',		    'majigsaw',		    'Mod creathor',	 'https://youtube.com/channel/UCmdcI4Ma8ZWDnG10QWlUv8g',	'0xFFC30085'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
